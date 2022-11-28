199442e0ee6096577170e64d95380729c6f3009f
crutoboy <vanyalex0192837465@gmail.com>
2022-11-28 21:45:02 +0300

    v1.0

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import file_working
 
 
 
-bot = telebot.TeleBot(token, 'MARKDOWN')
+bot = telebot.TeleBot(token)
 dict_with_active_explorers = {}
 
 
@@ -43,14 +43,14 @@ def open_new_explorer (message):
     if len(lstdir[0]) > 0 or len(lstdir[1]) > 0:
         tmp = False
     for i in lstdir[0]:
-        btn = types.InlineKeyboardButton(text = str(i), callback_data = f'cd|{i[2:]}')
+        btn = types.InlineKeyboardButton(text = str(i), callback_data = f'dir|cd|{i[2:]}')
         keaboard.add(btn)
     if visible:
         for i in lstdir[1]:
-            btn = types.InlineKeyboardButton(text = str(i), callback_data = f'file|{i}')
+            btn = types.InlineKeyboardButton(text = str(i), callback_data = f'file|open|{i}')
             keaboard.add(btn)
     if dir.text != '/' and tmp and (space or admin):
-        btn = types.InlineKeyboardButton(text = 'Удалить текущую папку', callback_data = 'remove_dir')
+        btn = types.InlineKeyboardButton(text = 'Удалить текущую папку', callback_data = 'dir|remove')
         keaboard.add(btn)
     tmp1 = bot.send_message(message.from_user.id, dir.text, reply_markup = keaboard)
 
@@ -75,7 +75,7 @@ def get_text_messages(message):
     if message.text == '⬆':
         dir = db_working.get_database(message.from_user.id)[1]
         if dir.text != '/':
-            message.update({'data': 'cd_back_keyboard|..'})
+            message.data = 'dir|cd|..'
             callback_worker(message)
         else:
             open_new_explorer (message)
@@ -150,13 +150,55 @@ def addfile(message):
     # bot.send_message(message.from_user.id, f'Файл был загружен по пути: {file_working.normpath(f"{path.text}/{file_name}")}', None, reply_to_message_id = message.message_id)
 
 
-
-
-
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    call_data = call.data.split('|')
+    if call_data[0] == 'update':
+        bot.delete_message(call.from_user.id, call.message.id)
+        open_new_explorer (call)
+    elif call_data[0] == 'file':
+        root, dir, space, _, admin = db_working.get_database(call.from_user.id)[:5]
+        if call_data[1] == 'open':
+            keaboard = types.InlineKeyboardMarkup()
+            keaboard.add(types.InlineKeyboardButton(text = 'Назад', callback_data = 'update'))
+            keaboard.add(types.InlineKeyboardButton(text = 'Скачать', callback_data = f'file|download|{call_data[2]}'))
+            if space or admin:
+                # keaboard.add(types.InlineKeyboardButton(text = 'Переименовать', callback_data = f'file|rename|{call_data[2]}'))
+                keaboard.add(types.InlineKeyboardButton(text = 'Удалить', callback_data = f'file|remove|{call_data[2]}'))
+            bot.send_message(call.from_user.id, file_working.normpath(f'{dir.text}/{call_data[2]}'), reply_markup = keaboard)
+        elif call_data[1] == 'download':
+            bot.send_document(call.from_user.id, open(file_working.get_path_to_file(f'{dir.text}/{call_data[2]}', space, call.from_user.id), 'rb'))
+        elif call_data[1] == 'rename':
+            ...
+        elif call_data[1] == 'remove':
+            keaboard = types.InlineKeyboardMarkup()
+            btn1 = types.InlineKeyboardButton(text = 'Да', callback_data = f'file|askremove|y|{call_data[2]}')
+            btn2 = types.InlineKeyboardButton(text = 'Нет', callback_data = 'file|askremove|n')
+            keaboard.add(btn1, btn2)
+            bot.send_message(call.from_user.id, 'Точно удалить файл?', reply_markup = keaboard)
+        elif call_data[1] == 'askremove':
+            bot.delete_message(call.from_user.id, call.message.id)
+            if call_data[2] == 'y':
+                os.remove(file_working.get_path_to_file(f'{dir.text}/{call_data[3]}', space, call.from_user.id))
+                bot.send_message(call.from_user.id, 'Файл удалён')
+            elif call_data[2] == 'n':
+                bot.send_message(call.from_user.id, 'Действие отменено')
+    elif call_data[0] == 'dir':
+        if call_data[1] == 'cd':
+            root, dir = db_working.get_database(call.from_user.id)[:2]
+            dir.text = file_working.normpath(f'{dir.text}/{call_data[2]}')
+            db_working.save_db(root)
+            open_new_explorer (call)
+        elif call_data[1] == 'remove':
+            root, dir, space = db_working.get_database(call.from_user.id)[:3]
+            os.rmdir(file_working.get_path_to_file(dir.text, space, call.from_user.id))
+            dir.text = file_working.normpath(f'{dir.text}/..')
+            db_working.save_db(root)
+            open_new_explorer (call)
 
 
 if __name__ == '__main__':
     os.chdir(root_path)
     db_working.check_db()
-    bot.polling(non_stop=True, interval=0)
-    # bot.infinity_polling()
+    # bot.polling(non_stop=True, interval=0)
+    bot.infinity_polling()
